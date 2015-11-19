e5f29056f4a70885a65bb62bd2d3fbf96183d50c
MironovVS <MAdd2062@gmail.com>
2015-11-19 13:10:20 +0400

    Исправили и оптимизировали работоспособность функций

--- model/function.php
+++ model/function.php
@@ -6,37 +6,37 @@
  * Time: 21:07
  */
 
+//Подключаемся к базе данных
 $server='localhost';
 $username='admin';
 $password='';
 mysql_connect($server, $username, $password);
-
 $db_name='PHP2_HT1-Mironov';
 mysql_select_db($db_name);
 
+
+//Функция добавления статьи
 function article_add($name, $date, $text){
   $request="INSERT INTO `Test`(`id` ,`article_name`, `article_date`, `article_text`) VALUES ('', '$name', '$date', '$text') ";
   mysql_query($request);
 }
 
+//Функция выводит все статьи
 function article_list() {
   $request=mysql_query("SELECT `id` ,`article_name`, `article_date`, `article_text` FROM `Test` ORDER BY `article_date` DESC");
   $art=array();
-
-
   while ($row=mysql_fetch_assoc($request)) {
     $art[]=$row;
   }
-
   return array($art);
 }
 
+//Функция выводит статью по id
 function article_search($id) {
   $id=addslashes($id);
   $sql="SELECT `id`,`article_name`, `article_date`, `article_text` FROM `Test` WHERE `id`='$id'";
   $request=mysql_query($sql);
   $search=array();
-
   While ($row=mysql_fetch_assoc($request)) {
     $search[]=$row;
   }
@@ -83,7 +83,8 @@ function article_search($id) {
 //  return array($up);
 //}
 
+// Функция редактирования статьи
 function article_up_up($id, $name, $text){
-  $request="UPDATE `Test` SET `article_name`='$name', `article_text`='$text' WHERE `id`=14";
+  $request="UPDATE `Test` SET `article_name`='$name', `article_text`='$text' WHERE `id`='$id'";
   mysql_query($request);
 }
