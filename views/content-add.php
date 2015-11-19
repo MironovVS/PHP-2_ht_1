8add2f188bb043af8654936fdf2d3b05dc979a96
MironovVS <MAdd2062@gmail.com>
2015-11-19 14:46:04 +0400

    добаил изменение в html

--- views/content-add.php
+++ views/content-add.php
@@ -1,8 +1,8 @@
 <fieldset>
   <form method="GET" name="article_add">
-    Название статьи: <input type="text" name="article_name">
-    Дата: <input type="date" name="article_date"><br>
-    Статья: <textarea name="article_text" rows="5"></textarea><br>
+    Название статьи: <input type="text" name="article_name"><br><br>
+    Дата: <input type="date" name="article_date"><br><br>
+    Статья: <textarea name="article_text" rows="5"></textarea><br><br>
     <input type="submit" name="submit" value="Сохранить">
   </form>
 </fieldset>
